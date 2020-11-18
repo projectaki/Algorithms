f492a6a1995fec289c3d5c6e147ee9e08d34bcfa
projectaki <akosegypro@gmail.com>
2020-11-18 18:42:23 +0100

    Find Next with comments

--- src/TreesAndGraphs/FindNext.java
+++ src/TreesAndGraphs/FindNext.java
@@ -5,6 +5,9 @@ package TreesAndGraphs;
  */
 public class FindNext {
 
+    /**
+     * Binary tree
+     */
     private static class Node {
         private int val;
         private Node left;
@@ -21,24 +24,32 @@ public class FindNext {
         }
     }
 
+    /**
+     * Helper class for passing up recursive calls
+     */
     private static class Next {
         private Node currentNode;
-        private boolean foundRight;
-        private boolean foundLeft;
+        private boolean passUp;
+        private boolean passUpWithoutRightSubTree;
 
         public Next() {
 
         }
 
+        public Next(Node cr, boolean fr) {
+            this.currentNode = cr;
+            this.passUp = fr;
+        }
+
         public Next(Node cr, boolean fr, boolean fl) {
             this.currentNode = cr;
-            this.foundLeft = fl;
-            this.foundRight = fr;
+            this.passUp = fr;
+            this.passUpWithoutRightSubTree = fl;
         }
     }
 
     /**
-     * REcursive Depth first search to find Next Node on BST
+     * Recursive Depth first search to find Next Node on BST
      * @param x Root of BST
      * @param Given Node to find the successor for
      * @return Returns the next Node
@@ -53,49 +64,58 @@ public class FindNext {
                     z = z.left;
                 }
                 // return most left Node, found right subtree = true
-                return new Next(z,true,false);
+                return new Next(z,true);
             }
             // if there is no right subtree, we have to pass up true for found, but not in right subtree
             else return new Next(null,false,true);
         }
-        if(x.left == null && x.right == null) return new Next(null, false, false);
+        if(x.left == null && x.right == null) return new Next(null, false);
 
         Next temp;
 
         if (x.left != null){
             temp = findNext(x.left, Given);
         }
-        else temp = new Next(null,false,false);
+        else temp = new Next(null,false);
 
         // if we found the next in right subtree then just pass it up the recursive call
-        if (temp.foundRight) return temp;
+        if (temp.passUp) return temp;
 
-        // if foundLeft == true then we set found right to true and return the current Node
-        // This happens if no right or left subtree and no parent to the right, meaning we have to skip a parent
-        if (temp.foundLeft) return new Next(x,true,false);
+        // if we are after a left search and we found in not right subtree, then we just return x, and set isRightSUb tree
+        // to true to pass it up
+        if (temp.passUpWithoutRightSubTree) return new Next(x,true);
 
         if (x.right != null){
             temp = findNext(x.right, Given);
         }
-        else temp = new Next(null,false,false);
+        else temp = new Next(null,false);
+
+        // if we found the next in right subtree then just pass it up the recursive call
+        if (temp.passUp) return temp;
+
+        // if we are after right search and we have a found but not in right subtree, then we cant go right,
+        // we have to go one up the recursive call, and then one up again, then return x;
+        // this is done by just setting the notRightSubtree to true again, which in the one up level will set the rightSub to true
+        // which will return the correct x
+        if (temp.passUpWithoutRightSubTree) return new Next(x,false,true);
 
         return temp;
     }
 
     public static void main(String[] args) {
-        Node e = new Node(55, null, null);
-        Node f = new Node(66, null, null);
+        //Node e = new Node(55, null, null);
+        //Node f = new Node(66, null, null);
         Node nine = new Node(9, null, null);
         Node seven = new Node(7, null, null);
         Node eight = new Node(8, seven, nine);
         Node five = new Node(5, null, null);
         Node three = new Node(3, null, null);
-        Node one = new Node(1, e, f);
+        Node one = new Node(1, null, null);
         Node six = new Node(6, five, eight);
         Node two = new Node(2, one, three);
         Node four = new Node(4,two,six);
 
-        System.out.println(findNext(four,f).currentNode.val);
+        System.out.println(findNext(four,three).currentNode.val);
     }
 
 }
